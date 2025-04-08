f10ccc7c4943d4af462d4aaac451129baa9470fb
Wojciech Broniarczyk <wojciechbroniarczyk@gmail.com>
2025-04-08 23:58:45 +0200

    add UI class with draw method

---
+++ src/main/java/main/UI.java
@@ -0,0 +1,76 @@
+package main;
+
+import java.awt.*;
+import java.util.ArrayList;
+
+import object.OBJ_Coin_Bronze;
+import object.OBJ_Heart;
+import object.OBJ_ManaCrystal;
+import entity.Entity;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.awt.image.BufferedImage;
+
+public class UI { // UI - User interface
+
+    GamePanel gp;
+    Graphics2D g2;
+    public Font purisaB;
+    BufferedImage heart_full, heart_half, heart_blank, crystal_full, crystal_blank, coin;
+    public boolean messageOn = false;
+    ArrayList<String> message = new ArrayList<>();
+    ArrayList<Integer> messageCounter = new ArrayList<>();
+    public boolean gameFinished = false;
+    public String currentDialogue = "";
+    public int commandNum = 0;
+    public int titleScreenState = 0; //  0: the first screen, 1: the second screen
+    public int playerSlotCol = 0;
+    public int playerSlotRow = 0;
+    public int npcSlotCol = 0;
+    public int npcSlotRow = 0;
+    int subState = 0;
+    int counter = 0;
+    public Entity npc;
+    int charIndex = 0;
+    String combinedText = "";
+
+    public UI(GamePanel gp) {
+        this.gp = gp;
+
+        try {
+            InputStream is = getClass().getResourceAsStream("/font/Purisa Bold.ttf");
+            purisaB = Font.createFont(Font.TRUETYPE_FONT, is);
+        } catch (FontFormatException | IOException e) {
+            e.printStackTrace();
+        }
+
+        // CREATE HUD OBJECT
+        Entity heart = new OBJ_Heart(gp);
+        heart_full = heart.image;
+        heart_half = heart.image2;
+        heart_blank = heart.image3;
+        Entity crystal = new OBJ_ManaCrystal(gp);
+        crystal_full = crystal.image;
+        crystal_blank = crystal.image2;
+        Entity bronzeCoin = new OBJ_Coin_Bronze(gp);
+        coin = bronzeCoin.down1;
+    }
+
+    public void addMessage(String text) {
+        message.add(text);
+        messageCounter.add(0);
+    }
+    public void draw(Graphics2D g2) {
+        this.g2 = g2;
+
+        g2.setFont(purisaB);
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+        g2.setColor(Color.white);
+
+
+    }
+
+
+}
