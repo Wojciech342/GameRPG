33dbbaeeece16541d40c443c0dbdd9d9d77e5a3e
Wojciech Broniarczyk <wojciechbroniarczyk@gmail.com>
2025-04-09 12:27:05 +0200

    add setting moving action

--- src/main/java/entity/NPC_OldMan.java
+++ src/main/java/entity/NPC_OldMan.java
@@ -50,7 +50,38 @@ public class NPC_OldMan extends Entity {
         dialogues[2][0] = "I wonder how to open thar door...";
 
     }
+    public void setAction() {
+        if(onPath) {
+            int goalCol = (gp.player.worldX + gp.player.solidArea.x)/gp.tileSize;
+            int goalRow = (gp.player.worldY + gp.player.solidArea.y)/gp.tileSize;
 
+            searchPath(goalCol, goalRow);
+        }
+        else {
+            actionLockCounter++;
+
+            if(actionLockCounter == 180) {
+                Random random = new Random();
+                int i = random.nextInt(100) + 1;// pick up a number from 1 to 100
+
+                if(i <= 25) {
+                    direction = "up";
+                }
+                else if(i > 25 && i <= 50) {
+                    direction = "down";
+                }
+                else if(i > 50 && i <= 75) {
+                    direction = "left";
+                }
+                else if(i > 75 && i <=100) {
+                    direction = "right";
+                }
+
+                actionLockCounter = 0;
+            }
+        }
+
+    }
     public void speak() {
         facePlayer();
         startDialogue(this, dialogueSet);
