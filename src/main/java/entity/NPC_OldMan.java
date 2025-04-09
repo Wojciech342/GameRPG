ccb53b50dba31634ca5fd7773857441adc1e3253
Wojciech Broniarczyk <wojciechbroniarczyk@gmail.com>
2025-04-09 12:23:22 +0200

    add old man object with image

---
+++ src/main/java/entity/NPC_OldMan.java
@@ -0,0 +1,41 @@
+package entity;
+
+import java.awt.Rectangle;
+import java.util.Random;
+
+import main.GamePanel;
+
+public class NPC_OldMan extends Entity {
+
+    public NPC_OldMan(GamePanel gp) {
+        super(gp);
+
+        direction = "down";
+        speed = 1;
+
+        solidArea = new Rectangle();
+        solidArea.x = 8;
+        solidArea.y = 16;
+        solidAreaDefaultX = solidArea.x;
+        solidAreaDefaultY = solidArea.y;
+        solidArea.width = 32;
+        solidArea.height = 32;
+
+        dialogueSet = -1; // Cause we add one before the method is called, so this is so that we start from index 0
+
+        getImage();
+    }
+
+    public void getImage() {
+
+        up1 = setup("/npc/oldman_up_1", gp.tileSize, gp.tileSize);
+        up2 = setup("/npc/oldman_up_2", gp.tileSize, gp.tileSize);
+        down1 = setup("/npc/oldman_down_1", gp.tileSize, gp.tileSize);
+        down2 = setup("/npc/oldman_down_2", gp.tileSize, gp.tileSize);
+        left1 = setup("/npc/oldman_left_1", gp.tileSize, gp.tileSize);
+        left2 = setup("/npc/oldman_left_2", gp.tileSize, gp.tileSize);
+        right1 = setup("/npc/oldman_right_1", gp.tileSize, gp.tileSize);
+        right2 = setup("/npc/oldman_right_2", gp.tileSize, gp.tileSize);
+    }
+    
+}
