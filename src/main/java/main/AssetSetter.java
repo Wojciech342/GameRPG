dc0a78e0b7682c62ae447d445181bc9409b5b5ad
Wojciech Broniarczyk <wojciechbroniarczyk@gmail.com>
2025-04-10 23:40:12 +0200

    add object placing on the maps

--- src/main/java/main/AssetSetter.java
+++ src/main/java/main/AssetSetter.java
@@ -1,8 +1,26 @@
 package main;
 
-
+import data.Progress;
+import entity.NPC_BigRock;
+import entity.NPC_Merchant;
 import entity.NPC_OldMan;
-
+import monster.MON_Bat;
+import monster.MON_GreenSlime;
+import monster.MON_RedSlime;
+import monster.MON_Orc;
+import monster.MON_SkeletonLord;
+import object.OBJ_Axe;
+import object.OBJ_BlueHeart;
+import object.OBJ_Chest;
+import object.OBJ_Door;
+import object.OBJ_Door_Iron;
+import object.OBJ_Key;
+import object.OBJ_Pickaxe;
+import object.OBJ_Shield_Blue;
+import object.OBJ_Tent;
+import tile_interactive.IT_DestructibleWall;
+import tile_interactive.IT_DryTree;
+import tile_interactive.IT_MetalPlate;
 
 public class AssetSetter {
     
@@ -11,14 +29,85 @@ public class AssetSetter {
     public AssetSetter(GamePanel gp) {
         this.gp = gp;
     }
+    public void setObject() {
 
+        int mapNum = 0;
+        int i = 0;
+
+        gp.obj[mapNum][i] = new OBJ_Tent(gp);
+        gp.obj[mapNum][i].worldX = gp.tileSize*28;
+        gp.obj[mapNum][i].worldY = gp.tileSize*39;
+        i++;
+        gp.obj[mapNum][i] = new OBJ_Axe(gp);
+        gp.obj[mapNum][i].worldX = gp.tileSize*35;
+        gp.obj[mapNum][i].worldY = gp.tileSize*32;
+        i++;
+        gp.obj[mapNum][i] = new OBJ_Door(gp);
+        gp.obj[mapNum][i].worldX = gp.tileSize*14;
+        gp.obj[mapNum][i].worldY = gp.tileSize*28;
+        i++;
+        gp.obj[mapNum][i] = new OBJ_Door(gp);
+        gp.obj[mapNum][i].worldX = gp.tileSize*12;
+        gp.obj[mapNum][i].worldY = gp.tileSize*12;
+        i++;
+        gp.obj[mapNum][i] = new OBJ_Chest(gp);
+        gp.obj[mapNum][i].setLoot(new OBJ_Key(gp));
+        gp.obj[mapNum][i].worldX = gp.tileSize*18;
+        gp.obj[mapNum][i].worldY = gp.tileSize*20;
+        i++;
+        gp.obj[mapNum][i] = new OBJ_Chest(gp);
+        gp.obj[mapNum][i].setLoot(new OBJ_Shield_Blue(gp));
+        gp.obj[mapNum][i].worldX = gp.tileSize*30;
+        gp.obj[mapNum][i].worldY = gp.tileSize*29;
+        i++;
+
+        mapNum = 2;
+        i = 0;
+        gp.obj[mapNum][i] = new OBJ_Chest(gp);
+        gp.obj[mapNum][i].setLoot(new OBJ_Pickaxe(gp));
+        gp.obj[mapNum][i].worldX = gp.tileSize*40;
+        gp.obj[mapNum][i].worldY = gp.tileSize*41;
+        i++;
+        gp.obj[mapNum][i] = new OBJ_Chest(gp);
+        gp.obj[mapNum][i].setLoot(new OBJ_Pickaxe(gp));
+        gp.obj[mapNum][i].worldX = gp.tileSize*13;
+        gp.obj[mapNum][i].worldY = gp.tileSize*16;
+        i++;
+
+        gp.obj[mapNum][i] = new OBJ_Chest(gp);
+        gp.obj[mapNum][i].setLoot(new OBJ_Pickaxe(gp));
+        gp.obj[mapNum][i].worldX = gp.tileSize*26;
+        gp.obj[mapNum][i].worldY = gp.tileSize*34;
+        i++;
+        gp.obj[mapNum][i] = new OBJ_Chest(gp);
+        gp.obj[mapNum][i].setLoot(new OBJ_Pickaxe(gp));
+        gp.obj[mapNum][i].worldX = gp.tileSize*27;
+        gp.obj[mapNum][i].worldY = gp.tileSize*15;
+        i++;
+        // gp.obj[mapNum][i] = new OBJ_Door_Iron(gp);
+        // gp.obj[mapNum][i].worldX = gp.tileSize*18;
+        // gp.obj[mapNum][i].worldY = gp.tileSize*23;
+        // i++;
+
+        mapNum = 3;
+        i = 0;
+        gp.obj[mapNum][i] = new OBJ_Door_Iron(gp);
+        gp.obj[mapNum][i].worldX = gp.tileSize*25;
+        gp.obj[mapNum][i].worldY = gp.tileSize*15;
+        i++;
+        gp.obj[mapNum][i] = new OBJ_BlueHeart(gp);
+        gp.obj[mapNum][i].worldX = gp.tileSize*25;
+        gp.obj[mapNum][i].worldY = gp.tileSize*8;
+        i++;
+
+    }
     public void setNPC() {
 
         // MAP 0
         int mapNum = 0;
         int i = 0;
         gp.npc[mapNum][i] = new NPC_OldMan(gp);
-        gp.npc[mapNum][i].worldX = gp.tileSize * 21;
-        gp.npc[mapNum][i].worldY = gp.tileSize * 21;
-    }
+        gp.npc[mapNum][i].worldX = gp.tileSize*21;
+        gp.npc[mapNum][i].worldY = gp.tileSize*21;
+        }
 }
