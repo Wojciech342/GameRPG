6fd7fff78ab435a47df112d81720706ba9daccfd
Wojciech Broniarczyk <wojciechbroniarczyk@gmail.com>
2025-04-13 23:55:10 +0200

    add stairs sound for changing map

--- src/main/java/main/Sound.java
+++ src/main/java/main/Sound.java
@@ -24,6 +24,8 @@ public class Sound {
         soundURL[10] = getClass().getResource("/sound/burning.wav");
         soundURL[11] = getClass().getResource("/sound/cuttree.wav");
         soundURL[12] = getClass().getResource("/res/sound/gameover.wav");
+        soundURL[13] = getClass().getResource("/res/sound/stairs.wav");
+
 
 
     }
