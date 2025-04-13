39fb328894a2965d452c3b195c00fea4bc9b4318
Wojciech Broniarczyk <wojciechbroniarczyk@gmail.com>
2025-04-13 23:59:30 +0200

    add merchant placement

--- src/main/java/main/AssetSetter.java
+++ src/main/java/main/AssetSetter.java
@@ -107,7 +107,17 @@ public class AssetSetter {
         int mapNum = 0;
         int i = 0;
         gp.npc[mapNum][i] = new NPC_OldMan(gp);
-        gp.npc[mapNum][i].worldX = gp.tileSize*21;
-        gp.npc[mapNum][i].worldY = gp.tileSize*21;
-        }
+        gp.npc[mapNum][i].worldX = gp.tileSize * 21;
+        gp.npc[mapNum][i].worldY = gp.tileSize * 21;
+        i++;
+
+        // MAP 1
+        mapNum = 1;
+        i = 0;
+        gp.npc[mapNum][i] = new NPC_Merchant(gp);
+        gp.npc[mapNum][i].worldX = gp.tileSize * 12;
+        gp.npc[mapNum][i].worldY = gp.tileSize * 7;
+        i++;
+
+    }
 }
