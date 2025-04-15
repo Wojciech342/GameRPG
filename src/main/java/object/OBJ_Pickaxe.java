60dad53d0893a9c52903433f8f1a94b9d7bb1274
Wojciech Broniarczyk <wojciechbroniarczyk@gmail.com>
2025-04-15 17:35:25 +0200

    add Pickaxe class

---
+++ src/main/java/object/OBJ_Pickaxe.java
@@ -0,0 +1,26 @@
+package object;
+
+import entity.Entity;
+import main.GamePanel;
+
+public class OBJ_Pickaxe extends Entity {
+
+    public static final String objName = "Pickaxe";
+
+    public OBJ_Pickaxe(GamePanel gp) {
+        super(gp);
+
+        type = type_pickaxe;
+        name = objName;
+        down1 = setup("/objects/pickaxe", gp.tileSize, gp.tileSize);
+        attackValue = 2;
+        attackArea.width = 30;
+        attackArea.height = 30;
+        description = "[Pickaxe]\nYou will dig.";
+        price = 40;
+        knockBackPower = 5;
+        motion1_duration = 10;
+        motion2_duration = 20;
+    }
+    
+}
